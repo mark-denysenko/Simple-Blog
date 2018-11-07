e1f482838590234ddaa33ca24b46103fa52506f5
Mark <mark.denyssenko@gmail.com>
2018-11-07 17:48:44 +0300

    Simplidied photo helper

--- WebUI/HtmlHelpers/PhotoHelper.cs
+++ WebUI/HtmlHelpers/PhotoHelper.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Web;
 using System.Web.Mvc;
 using WebUI.Util;
 
@@ -13,24 +9,14 @@ namespace WebUI.HtmlHelpers
     {
         public static string ShowUserPhoto(this HtmlHelper helper, string userName)
         {    
-            var userPhotoPath = Path.Combine(MyConfiguration.USER_AVATAR_PATH, userName);
-            userPhotoPath = Directory.GetFiles(MyConfiguration.USER_AVATAR_FULL_PATH, userName + "*").FirstOrDefault()?.Split(new[] {'\\', '/' })?.Last();
+            string userPhoto = Directory.GetFiles(MyConfiguration.USER_AVATAR_FULL_PATH, userName + "*")
+                .FirstOrDefault()
+                ?.Split('\\', '/')
+                .Last();
 
-            FileInfo userPhoto = new FileInfo(userPhotoPath ?? MyConfiguration.NO_PHOTO_PATH);
-
-            TagBuilder tag = new TagBuilder("img");
-            tag.MergeAttribute("src", userPhoto.FullName);
-            tag.MergeAttribute("height", MyConfiguration.PHOTO_HEIGHT);
-            tag.MergeAttribute("width",  MyConfiguration.PHOTO_WIDTH);
-            tag.MergeAttribute("alt", "user photo");
-            tag.AddCssClass("user-photo");
-
-            //tag.MergeAttribute("referrerpolicy", "no-referrer-when-downgrade");
-
-            //return MvcHtmlString.Create(tag.ToString());
-            if (userPhotoPath != null)
-                userPhotoPath = "/" + MyConfiguration.USER_AVATAR_FOLDER + "/" + userPhotoPath;
-            return userPhotoPath ?? MyConfiguration.NO_AVATAR_PIC;
+            return userPhoto == null 
+                ? MyConfiguration.NO_PHOTO_PATH
+                : MyConfiguration.USER_AVATAR_PATH + userPhoto;
         }
     }
 }
--- WebUI/Util/MyConfiguration.cs
+++ WebUI/Util/MyConfiguration.cs
@@ -9,9 +9,9 @@ namespace WebUI.Util
     {
         public const string NO_AVATAR_PIC = "NoAvatar.png";
         public const string USER_AVATAR_FOLDER = "UserAvatars";
-        public const string USER_AVATAR_PATH = "~/Content/images/" + USER_AVATAR_FOLDER + "/";
+        public const string USER_AVATAR_PATH = "/Content/images/" + USER_AVATAR_FOLDER + "/";
         public const string USER_AVATAR_FULL_PATH = @"C:\Users\Mark\source\repos\NTierSolution\WebUI\Content\images\" + USER_AVATAR_FOLDER + "\\";
-        public const string NO_PHOTO_PATH = "~/Content/images/" + NO_AVATAR_PIC;
+        public const string NO_PHOTO_PATH = "/Content/images/" + NO_AVATAR_PIC;
         public const string PHOTO_HEIGHT = "400";
         public const string PHOTO_WIDTH = "400";
         public const int POST_PER_PAGE = 5;
